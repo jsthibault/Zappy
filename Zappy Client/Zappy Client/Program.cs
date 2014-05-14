044be7298bdc3d886f65abbee6e5735f01366049
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2014-05-14 11:07:26 +0200

    Commit .gitignore

--- Zappy Client/Zappy Client/Program.cs
+++ Zappy Client/Zappy Client/Program.cs
@@ -1,25 +1,17 @@
-﻿#region Using Statements
-using System;
-using System.Collections.Generic;
-using System.Linq;
-#endregion
+﻿using System;
 
 namespace Zappy_Client
 {
 #if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             using (var game = new Game1())
+            {
                 game.Run();
+            }
         }
     }
 #endif
