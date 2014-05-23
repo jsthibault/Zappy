a70833d97331937f77062cf8f42f446f17770a31
Drain <arnaud.drain@epitech.eu>
2014-05-23 14:19:17 +0200

    base server

---
+++ include/server.h
@@ -0,0 +1,44 @@
+/*
+** serveur.h for serveur in /home/drain_a/rendu/PSU_2013_myirc
+** 
+** Made by arnaud drain
+** Login   <drain_a@epitech.net>
+** 
+** Started on  Fri Apr 18 18:42:10 2014 arnaud drain
+** Last update Tue May 13 19:40:00 2014 arnaud drain
+*/
+
+#ifndef SERVEUR_H_
+# define SERVEUR_H_
+
+# define BUFFER_SIZE	(4096)
+
+# include "options.h"
+
+typedef struct s_client	t_client;
+
+struct		s_client
+{
+  int		fd;
+  char		*name;
+  char		*ip;
+  t_client	*next;
+};
+
+typedef struct	s_functions
+{
+  char		*name;
+  int		(*function)(char **, t_client *);
+}		t_functions;
+
+char	**my_str_to_wordtab(char *str);
+void	freetab(char **tab);
+int	launch_srv(t_options *options);
+int	return_error(char *str, int option);
+int	write_socket(int fd, char *str);
+int	init(size_t port);
+int	add_client(int sfd, t_client **clients);
+void	pop_client(int fd, t_client **clients);
+int	graphic(char **, t_client *);
+
+#endif /* !SERVEUR_H_ */
