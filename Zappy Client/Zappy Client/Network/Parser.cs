7d3b02546f6d122bb0a8ba46e28ee4059dff0366
Samy Ouachek <samy.ouachek@epitech.eu>
2014-07-05 19:24:53 +0200

    New regex for commands

--- Zappy Client/Zappy Client/Network/Parser.cs
+++ Zappy Client/Zappy Client/Network/Parser.cs
@@ -34,28 +34,27 @@ namespace Zappy_Client
         {
             AuthorizedItems = new List<Regex>();
             AuthorizedItems.Add(new Regex("^msz [\\d]* [\\d]*\n$"));
-            AuthorizedItems.Add(new Regex("^msz [\\d]* [\\d]*\\n$"));
-            AuthorizedItems.Add(new Regex("^bct \\d* \\d* \\d* \\d* \\d* \\d* \\d* \\d*\\n$"));
+            AuthorizedItems.Add(new Regex("^bct( \\d*){8}\n$"));
             AuthorizedItems.Add(new Regex("^tna \\w*\\n$"));
-            AuthorizedItems.Add(new Regex("^pnw #\\d* \\d* \\d* [N|O|S|E]:\\d \\d* \\w*\\n$"));
-            AuthorizedItems.Add(new Regex("^ppo #\\d* \\d* \\d* [N|O|S|E]:\\d\\n$"));
+            AuthorizedItems.Add(new Regex("^pnw #\\d*( \\d*){2} [N|O|S|E]:\\d \\d* \\w*\\n$"));
+            AuthorizedItems.Add(new Regex("^ppo #\\d*( \\d*){2} [N|O|S|E]:\\d\\n$"));
             AuthorizedItems.Add(new Regex("^plv #\\d* \\d*\\n$"));
-            AuthorizedItems.Add(new Regex("^pin #\\d* \\d* \\d* \\d* \\d* \\d* \\d* \\d* \\d* \\d*\\n$"));
+            AuthorizedItems.Add(new Regex("^pin #\\d*( \\d*){9}\\n$"));
             AuthorizedItems.Add(new Regex("^pex #\\d*\\n$"));
-            AuthorizedItems.Add(new Regex("^pbc #\\d* .*\n$"));
-            AuthorizedItems.Add(new Regex("^smg .*\n$"));
-            AuthorizedItems.Add(new Regex("^pie \\d* \\d* [0|1]\\n$"));
+            AuthorizedItems.Add(new Regex("^pbc #\\d* .*\\n$"));
+            AuthorizedItems.Add(new Regex("^smg .*\\n$"));
+            AuthorizedItems.Add(new Regex("^pie( \\d*){2}[0|1]\\n$"));
             AuthorizedItems.Add(new Regex("^pfk #\\d*\\n$"));
             AuthorizedItems.Add(new Regex("^pdr #\\d* \\d*\\n$"));
             AuthorizedItems.Add(new Regex("^pgt #\\d* \\d*\\n$"));
             AuthorizedItems.Add(new Regex("^pdi #\\d*\\n$"));
-            AuthorizedItems.Add(new Regex("^enw #\\d* #\\d* \\d* \\d*\\n$"));
+            AuthorizedItems.Add(new Regex("^enw( #\\d*){2}( \\d*){2}\\n$"));
             AuthorizedItems.Add(new Regex("^eht #\\d*\\n$"));
             AuthorizedItems.Add(new Regex("^ebo #\\d*\\n$"));
             AuthorizedItems.Add(new Regex("^edi #\\d*\\n$"));
             AuthorizedItems.Add(new Regex("^sgt \\d*\\n$"));
             AuthorizedItems.Add(new Regex("^seg \\w*\\n$"));
-            AuthorizedItems.Add(new Regex("^pic \\d* \\d* \\d*( #\\d{1,}){1,}\n$"));
+            AuthorizedItems.Add(new Regex("^pic \\d* \\d* \\d*( #\\d{1,}){1,}\\n$"));
             AuthorizedItems.Add(new Regex("^suc\\n$"));
             AuthorizedItems.Add(new Regex("^sbp\\n$"));
         }
