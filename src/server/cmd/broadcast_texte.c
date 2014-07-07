ce0ee27ec338878b49500564e764075e95a184d2
loic lefloch <l.lefloch.91@gmail.com>
2014-07-07 15:35:33 +0200

    first version of broadcast

--- src/server/cmd/broadcast_texte.c
+++ src/server/cmd/broadcast_texte.c
@@ -5,7 +5,7 @@
 ** Login <lefloc_l@epitech.eu>
 **
 ** Started on  ven. mai 16 17:39:57 2014 lefloc_l
-** Last update Thu Jun 26 16:26:11 2014 arnaud drain
+** Last update lun. juil. 07 15:34:00 2014 lefloc_l
 */
 
 #include "client_action.h"
@@ -14,10 +14,31 @@
 #include "enum.h"
 #include "map.h"
 
+static void	send_to_clients(t_kernel *kernel, char *buffer)
+{
+  t_node	*node;
+  t_client	*list;
+
+  list = kernel->clients;
+  while (list)
+  {
+    write_socket(list->fd, buffer);
+    list = list->next;
+  }
+}
+
 int		cmd_broadcast_texte(char **av, t_client *cl, t_kernel *kernel)
 {
+  char		buffer[BUFFER_SIZE];
+  int		i;
+
   (void)av;
-  (void)cl;
-  (void)kernel;
+  sprintf(buffer, "message %d,", cl->player->orientation);
+  send_to_clients(kernel, buffer);
+  for (i = 1; av[i]; i++)
+  {
+    send_to_clients(kernel, av[i]);
+  }
+  send_to_clients(kernel, "\n");
   return (0);
 }
